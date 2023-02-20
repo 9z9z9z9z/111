07e37cfbc1f5bb3109e8dcf6aaa770e4142728af
9z9z9z9z <72688414+9z9z9z9z@users.noreply.github.com>
2023-02-20 17:44:48 +0300

    Add files via upload

---
+++ 8211816.ru/newEmptyPHP.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Задание 2</title>
+<style type="text/css">
+body {
+    font: 0.8em Arial, Helvetica, sans-serif; 
+    background: #000000; 
+    color: #ffffff; 
+}
+.header { font-size: 2em; text-align: center; padding: 5px; background: #8B0000; }
+.layout { background: #000000; overflow: hidden; min-width:600px; }
+.leftCol { background: #C0C0C0; float: left; margin: 0 180px 0 180px; padding: 5px; }
+.centerCol { background: #808080; margin: 0 180px; padding: 5px; }
+.rightCol { background: #696969; float: right; width: 180px; padding: 5px; }
+</style>
+</head>
+<body>
+<div class="header">Хомыженко Николай</div>
+<div class="layout">
+<div class="leftCol">Колонка 1</div>
+<div class="rightCol">Колонка 3</div>
+<div class="centerCol">Колонка 2</div>
+</div>
+</body>
+</html>
---
+++ 8211816.ru/try.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <meta http-equiv="Conetnt-Type" content="text/html; charset=utf-8" />
+        <title>Задание 2</title>
+        <style type="text/css">
+            body {
+                font: 0.8em Arial, Helvetica, sans-serif; 
+                background: #000000; 
+                color: #ffffff; 
+            }
+            #header {
+                font-size: 2em;
+                text-align: center;
+                padding: 5px; 
+                background: #8B0000; 
+            }
+            #leftCol {
+                background: #696969;
+                width: 180px;
+                float: left;
+            }
+            #rightCol {
+                background: #696969;
+                width: 180px;
+                float: right;
+            }
+            #cenCol { 
+                background: #696969;
+                margin: 0 180px 0 180px;
+                padding: 10px;
+            }
+        </style>
+    </head>
+    
+    <body>
+    <div id="body">
+        <div id="header"><span style="font-family: Impact;">Хомыженко Николай</span></div>
+        <div id="leftCol">Левая колонка</div>
+        <div id="cenCol">Центральная колонка</div>
+        <div id="cenCol">
+            <h3>Интересная колонка...</h3>
+        </div>
+        <div id="rightCol">Правая колонка</div>
+    </div>
+    </body>
+</html>
